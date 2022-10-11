c826d24a35023b4ea5bb2fb3b9001bc6a598d61f
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-10-11 13:23:08 +0900

    added heroku config options for sequelize

---
+++ config/config.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+
+module.exports = {
+  "development": {
+    host: DB_HOST,
+    database: 'course_database_dev',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  "test": {
+    host: DB_HOST,
+    database: 'course_database_test',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        required: true,
+        rejectUnauthorize: false
+      }
+    }
+  },
+  "production": {
+    host: DB_HOST,
+    database: 'course_database_prod',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        required: true,
+        rejectUnauthorize: false
+      }
+    }
+  },
+};
