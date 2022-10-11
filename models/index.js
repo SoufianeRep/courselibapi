ad6d87e5a3cd3752c8d39731b65a71cadf311645
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-10-11 17:55:16 +0900

    config modification

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -8,17 +8,23 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+let sequelize = new Sequelize(config.use_env_variable, {
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
+
+);
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
 
 fs.readdirSync(__dirname)
   .filter((file) => {
